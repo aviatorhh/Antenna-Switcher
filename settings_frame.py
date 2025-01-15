8c8c9db9d78c479160352eb189a07cb1015b5728
Nicholas John Koch <njk@pilot.hamburg>
2025-01-15 10:20:58 +0100

    Fiddling with the layout.

--- settings_frame.py
+++ settings_frame.py
@@ -8,7 +8,22 @@ class SettingsFrame(wx.Frame):
     
         wx.Frame.__init__(self, None, title=title, size=(320, 480), style=wx.CAPTION | wx.SYSTEM_MENU | wx.CLOSE_BOX)
 
+        panel = wx.Panel(self)
 
+        vbox = wx.BoxSizer(wx.VERTICAL)
+        panel.SetSizer(vbox)
+        buttonsBox = wx.StdDialogButtonSizer()
+        vbox.Add(buttonsBox, 1,
+                  wx.ALIGN_RIGHT | wx.LEFT | wx.RIGHT)
+        cancelButn = wx.Button(panel, wx.ID_CANCEL)
+        buttonsBox.AddButton(cancelButn)
+        okButn = wx.Button(panel, wx.ID_OK)
+        buttonsBox.AddButton(okButn)
+        okButn.SetDefault()
+        buttonsBox.Realize()
+
+        #cancelButn.Bind(wx.EVT_BUTTON, self.OnCancel)
+        #okButn.Bind(wx.EVT_BUTTON, self.OnOK)
 
 # for testing only
 if __name__ == '__main__':
